362d66f182489a2cf8950e99a5bd319fa9a9d769
taffikuddinkhan <taffikuddink@gmail.com>
2026-08-30 23:22:12 +0530

    discovered some new things

--- Hello.py
+++ Hello.py
@@ -1,1 +1,8 @@
 print("Hello world")
+print("My name is Taffikuddin khan" , "u can call me chand")
+print(15+15)
+name = "TAFFIK"
+age = 15
+print("I am" , name , "My age is " , age)
+print(type(name))
+print(type(age))
