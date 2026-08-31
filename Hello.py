b6d48e7fc10ee643e2786ca8958fde5f149d31ed
taffikuddinkhan <taffikuddink@gmail.com>
2026-08-31 06:54:26 +0530

    learned some nwe syntax and one line codes

--- Hello.py
+++ Hello.py
@@ -6,3 +6,18 @@ age = 15
 print("I am" , name , "My age is " , age)
 print(type(name))
 print(type(age))
+
+#Expression Execution --------------------------------------------
+
+sym = "@"
+a,b = 2,3
+c = "2"
+print(a*sym*b)
+print((c+sym)*b)
+
+#input in python --------------------------------------------------
+
+nam = input("name : ")
+agee = int(input("Enter age : "))
+Balance = float(input("Enter ur current balance : "))
+print("The user having name ",nam," having the current balance ",Balance," at just in ",agee," years old")
