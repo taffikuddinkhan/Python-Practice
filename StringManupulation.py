c802ff238d5eac4eb0dd9a4528110c7ecbe1741f
taffikuddinkhan <taffikuddink@gmail.com>
2026-09-06 20:14:04 +0530

    leran some new concepts

---
+++ StringManupulation.py
@@ -0,0 +1,12 @@
+str = "data scientist career path"
+print(str.endswith("th"))   # check that string ends with particular value or not
+str = str.replace("scientist","Analysis") # Replace a word or sentence or character
+print(str)
+print(str[-3:-1]) # Reverse index access
+
+
+#practice ---------------------------------------------------------------------
+#WAP TO ENTER UR NAME AND PRINT THE LENGTH OF UR NAME
+name = input("Enter your name : ")
+print("Your name length is : ",len(name))
+
