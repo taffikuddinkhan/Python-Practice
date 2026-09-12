5a1562adb5d4bd36a1041c1044aaa2acdf0bf4f0
taffikuddinkhan <taffikuddink@gmail.com>
2026-09-12 17:49:24 +0530

    started learnning oops concepts

--- oops.py
+++ oops.py
@@ -1,5 +1,13 @@
 class Student:  # student class
-    name="karan aujla" #property
 
-s1 = Student() #object creation here s1 is the object
-print(s1.name) #printing name field using the s1 object
+    #parameterized constructor
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks=marks
+
+
+s1 = Student("karan aujla",89) #object creation here s1 is the object , and passing value to be initialized by the __init__ to the object
+print(s1.name,s1.marks) #printing name and marks field using the s1 object
+
+s2 = Student("Zfar Bhai",98)
+print(s2.name,s2.marks)
