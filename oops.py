672eb4268509021e8c0d2c3435489b32621641da
taffikuddinkhan <taffikuddink@gmail.com>
2026-09-14 23:16:09 +0530

    started learnning oops concepts

--- oops.py
+++ oops.py
@@ -9,5 +9,5 @@ class Student:  # student class
 s1 = Student("karan aujla",89) #object creation here s1 is the object , and passing value to be initialized by the __init__ to the object
 print(s1.name,s1.marks) #printing name and marks field using the s1 object
 
-s2 = Student("Zfar Bhai",98)
+s2 = Student("Zfar Bhai",98) #we can create multiple student type objects
 print(s2.name,s2.marks)
