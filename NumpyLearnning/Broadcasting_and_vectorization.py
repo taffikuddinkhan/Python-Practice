8a5de59fba1008d124199eda5d37a5196864f1c5
taffikuddinkhan <taffikuddink@gmail.com>
2026-09-22 15:45:37 +0530

    learnning broadcasting in array using numpy

---
+++ NumpyLearnning/Broadcasting_and_vectorization.py
@@ -0,0 +1,16 @@
+# without using for loop performing a single operation on all the elements is known as broadcasting
+import numpy as np
+
+price = np.array([100,200,300])
+discount = 10
+final_price = price - (price * discount/100 )  # substraction 10% from each of the elements
+print(final_price)
+
+print(price * 2) # we don't need to iterate through each element and perform the same operation , instead of that numpy provides broadcasting
+
+print()
+matrix = np.array([[1,2,3],[4,5,6]])
+array = np.array([10,20,30])
+
+print(matrix + array)  # broadcasting , added array elements throughout each of the matrix element
+print(matrix * array)
